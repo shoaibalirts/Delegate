4a97456030e5ca86a0e1463d26a2c31fda35e8e9
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-14 14:39:03 +0200

    Add project files.

---
+++ ClassLibrary1/Sample.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class Sample
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public delegate int Comparison(int a, int b);
+}
